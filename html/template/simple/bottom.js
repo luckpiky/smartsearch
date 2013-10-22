bcd687908e2ac5a8bad380aafb379579875d587b
luckpiky <luckpiky@163.com>
2013-10-22 22:30:48 +0800

    增加链接以及百度统计

--- html/template/simple/bottom.js
+++ html/template/simple/bottom.js
@@ -1,1 +1,5 @@
 document.write("@@@@@");
+
+//百度统计
+var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3Fd0a9249d63662e8ea0e61c573722901b' type='text/javascript'%3E%3C/script%3E"));
